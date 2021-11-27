7a95ee0e32b010d4b870ac836f94f9488592c662
Code Sandbox <hello@codesandbox.io>
2021-11-27 20:26:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.640642193a3567c9e858726b870416d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f88a7b55e15b0dc8f1dd6a179a1b98",
+    "url": "https://kundy11.github.io/csb-km52l/index.html"
+  },
+  {
+    "revision": "ad8a91377e7fc272f3cd",
+    "url": "https://kundy11.github.io/csb-km52l/static/js/2.05d2717a.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://kundy11.github.io/csb-km52l/static/js/2.05d2717a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d047b823ce1057d09a8",
+    "url": "https://kundy11.github.io/csb-km52l/static/js/main.a4aa0db6.chunk.js"
+  },
+  {
+    "revision": "ee184144e1683ecbc89a",
+    "url": "https://kundy11.github.io/csb-km52l/static/js/runtime-main.15e1fd9e.js"
+  }
+]);
